68d30138d1ff842a6fc23f431b5105a201ded61e
Race Williams <race@racewilliams.com>
2024-02-13 17:56:49 -0500

    wasm alert to js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,17 @@
-<h1>content</h1>
+<script lang="ts">
+  import init, { greet } from "wasm";
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    await init();
+  });
+</script>
+
+<div>
+  <button
+    on:click={() => {
+      greet("hello");
+    }}>Click Me</button
+  >
+</div>
+
