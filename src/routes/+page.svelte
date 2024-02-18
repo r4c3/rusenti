78d223710752ded7b84766a2f3f6621498fc264a
apatel896 <patelaryan1013@gmail.com>
2024-02-18 15:01:26 -0500

    add skeleton for google auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
   import init, { greet } from "wasm";
   import { onMount } from "svelte";
-
+  import getGoogleOAuthURL from "$lib";
   onMount(async () => {
-    await init();
+     await init();
   });
 </script>
 
 <div>
   <button
     on:click={() => {
-      greet("hello");
+     // greet("hello");
     }}>Click Me</button
   >
+  <a href={getGoogleOAuthURL()}>loginWGoogle</a>
 </div>
