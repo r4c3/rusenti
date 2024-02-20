e6366f0d43f58e241612f8e62589b40cebe9ad54
Race Williams <race@racewilliams.com>
2024-02-20 16:31:00 -0500

    landing page exists

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,58 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+
+  const startBuilding = () => {
+    goto("/canvas");
+  };
 </script>
 
-<div>
-  <h1>Skru</h1>
-  <p>A web based paletted bitmap editor</p>
-  <p>• GPU-accelerated canvas rendering</p>
-  <p>• Server-side rendered static components</p>
-  <p>• Export to .png, .jpg, .c, and more</p>
+<div id="screen">
+  <div id="content">
+    <h1>Skru</h1>
+    <p>A paletted bitmap editor for the web.</p>
+    <p class="bullet">• GPU-accelerated canvas rendering</p>
+    <p class="bullet">• Server-side rendered static components</p>
+    <p class="bullet">• Export to .png, .jpg, .c, and more</p>
+    <button on:click={startBuilding}> Start building --></button>
+  </div>
 </div>
 
 <style>
+  #screen {
+    width: 100%;
+    height: 100%;
+    padding: 9% 8%;
+  }
+  #content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: fit-content;
+    height: fit-content;
+  }
   h1 {
-    font-size: 12rem;
+    font-size: 15rem;
     font-weight: 900;
-    margin-bottom: -1.2rem;
   }
   p {
-    font-size: 2rem;
+    font-size: 3.4rem;
+    margin-bottom: 0.3rem;
     font-weight: 600;
+  }
+  .bullet {
+    margin-left: 1rem;
+  }
+  button {
+    font-size: 3.4rem;
+    background: var(--white);
+    border: none;
+    border-radius: 10rem;
+    color: var(--black);
+    cursor: pointer;
+    width: fit-content;
+    padding: 0.5rem 2rem;
+    font-family: inherit;
+    font-weight: 700;
+    margin-top: 3.7rem;
   }
 </style>
