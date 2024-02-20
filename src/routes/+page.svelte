2d704b3fc8fff3950fd6c51d48d58c3e4a50fd6e
Race Williams <race@racewilliams.com>
2024-02-20 15:50:39 -0500

    page container styles, some setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-  import init, { greet } from "wasm";
-  import { onMount } from "svelte";
-
-  onMount(async () => {
-    await init();
-  });
 </script>
 
 <div>
-  <button
-    on:click={() => {
-      greet("hello");
-    }}>Click Me</button
-  >
+  <h1>Skru</h1>
+  <p>A web based paletted bitmap editor</p>
+  <p>• GPU-accelerated canvas rendering</p>
+  <p>• Server-side rendered static components</p>
+  <p>• Export to .png, .jpg, .c, and more</p>
 </div>
+
+<style>
+  h1 {
+    font-size: 12rem;
+    font-weight: 900;
+    margin-bottom: -1.2rem;
+  }
+  p {
+    font-size: 2rem;
+    font-weight: 600;
+  }
+</style>
