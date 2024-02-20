23219637349c8116bd15f011df5c2bcba039c890
Race Williams <race@racewilliams.com>
2024-02-20 16:39:49 -0500

    setup canvas file structure (wip)

--- src/lib/components/form.svelte
+++
@@ -1,6 +0,0 @@
-<form>
-    <label for="user">Username:</label><br>
-    <input type="text" id="username" name="user"><br>
-    <label for="pass">Password:</label><br>
-    <input type="text" id="password" name="pass">
-</form>
